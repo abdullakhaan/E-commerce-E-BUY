33fbd42cf280a9fdcb910c5eed3aa4399b9d56a2
Muhammad Abdullah Khan <abdullah.khana7993@gmail.com>
2024-11-10 22:02:02 +0500

    .env added

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -1,16 +1,16 @@
-// Import the functions you need from the SDKs you need
+// firebase.config.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAZ5eV6W2YxX7xRdYxkVktDCBSOHDw7pdc",
-  authDomain: "ecommerce-c8598.firebaseapp.com",
-  projectId: "ecommerce-c8598",
-  storageBucket: "ecommerce-c8598.appspot.com",
-  messagingSenderId: "370669819765",
-  appId: "1:370669819765:web:54d51fed570f97bc58a53c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -18,4 +18,4 @@ const app = initializeApp(firebaseConfig);
 const fireDB = getFirestore(app);
 const auth = getAuth(app);
 
-export {fireDB,auth } ;
+export { fireDB, auth };
